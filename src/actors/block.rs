7a0947e1961da97a095fe552ec55447d972ace48
Yoshinari Nomura <nom@nomlab.org>
2020-10-24 22:37:31 +0900

    Add soft-block feature

--- src/actors/block.rs
+++ src/actors/block.rs
@@ -1,4 +1,5 @@
 use crate::actors::*;
+use crate::game_state::*;
 use crate::geometry::*;
 use crate::*;
 
@@ -7,25 +8,59 @@ use crate::*;
 pub struct Block {
     actor_id: ActorId,
     action: u32,
+    ttl: i32,
     pub pnt: Point,
 }
 
 impl Block {
     pub fn new(x: i32, y: i32) -> Self {
-        Block {
-            actor_id: ActorId::Block,
-            action: 0,
-            pnt: grd!(x, y),
-        }
+        Block::hard(x, y)
+    }
+
+    pub fn hard(x: i32, y: i32) -> Self {
+        Block::build(x, y, 15, 0)
+    }
+
+    pub fn soft(x: i32, y: i32) -> Self {
+        Block::build(x, y, 14, 1)
+    }
+
+    pub fn is_soft(&self) -> bool {
+        self.ttl < 15
     }
 
     pub fn alive(&self) -> bool {
-        true
+        self.ttl > 0
     }
 
     pub fn draw(&self) {
         screen_put_sprite(self.pnt.x, self.pnt.y, self.actor_id, self.action)
     }
 
-    pub fn update(&mut self, _delta: i32) {}
+    pub fn update(&mut self, _delta: i32, gs: &GameState) {
+        // hardblock →nothing to do.
+        if self.ttl > 14 {
+            return;
+        }
+        // softblock → check if fired
+        if self.ttl == 14 {
+            let fire_exists = gs.fires().iter().any(|f| f.pnt == self.pnt);
+            if fire_exists {
+                self.ttl -= 1;
+            }
+        } else {
+            // burning the block
+            self.ttl -= 1;
+        }
+        self.action = (15 - self.ttl) as u32;
+    }
+
+    fn build(x: i32, y: i32, ttl: i32, action: u32) -> Self {
+        Block {
+            actor_id: ActorId::Block,
+            action,
+            ttl,
+            pnt: grd!(x, y),
+        }
+    }
 }
